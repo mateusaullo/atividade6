fa05c201f2faaf5f938dd9c6d994efd5540306ee
Mateus Saullo <mateusaullo13@gmail.com>
2023-04-05 02:07:30 -0300

    primeiro commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        double circulo;
+        double pi;
+        double resultado;
+
+        pi = 3.14159;
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Coloque o raio do circulo: ");
+        circulo = teclado.nextInt();
+        teclado.close();
+
+        resultado = pi * (circulo*circulo);
+
+        System.out.printf("A area do circulo eh: %.3f",resultado);
+    }
+}
